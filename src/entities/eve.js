87b64fba765077c5a7ac772745f39fd7111fb038
Hank Yates <hank@c4tech.com>
2013-01-14 23:36:16 -0600

    adding two way controls and animations

--- src/entities/eve.js
+++ src/entities/eve.js
@@ -7,11 +7,15 @@ Eve = Backbone.Model.extend({
 
     entity
       .attr({x: 648, y: 266, z: 300, w: 75, h:220})
-      .multiway(2, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
+      .multiway(2, {RIGHT_ARROW: 0, LEFT_ARROW: 180})
       .bind('KeyDown', function(e) {
-        if(e.key == Crafty.keys['RIGHT_ARROW']){
-          this.animate('walk_right', 0, 0, 9)
-          this.animate('walk_right', 0, -1)
+        if(e.key === Crafty.keys['RIGHT_ARROW']){
+          this.animate('walk_right', 0, 0, 9);
+          this.animate('walk_right', 0, -1);
+        }
+        if(e.key === Crafty.keys['LEFT_ARROW']){
+          this.animate('walk_left', 0, 1, 9);
+          this.animate('walk_left', 0, -1);
         }
       })
       .bind('KeyUp', function(e) {
