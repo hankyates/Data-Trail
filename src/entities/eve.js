fd32106c5a30bca74ade5fa4b00dd478485e4d26
Hank Yates <hank@c4tech.com>
2013-01-14 22:54:02 -0600

    moved eve

--- src/entities/eve.js
+++ src/entities/eve.js
@@ -6,7 +6,7 @@ Eve = Backbone.Model.extend({
     var entity = this.get('entity');
 
     entity
-      .attr({x: 650, y: 20, z: 300, w: 75, h:220})
+      .attr({x: 648, y: 266, z: 300, w: 75, h:220})
       .multiway(2, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
       .bind('KeyDown', function(e) {
         if(e.key == Crafty.keys['RIGHT_ARROW']){
